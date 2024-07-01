1c08808978f812b313a18e940ff735b3e611cbc1
Kishore P <kidkrrish3@gmail.com>
2024-07-01 08:06:55 +0530

    added website desc and title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,16 @@ import HomeScreen from "./Main/HomeScreen/HomeScreen";
 import HomeContent from "./Main/HomeContent/HomeContent";
 import Footer from "./Main/Footer/Footer";
 import "./styles/index.css";
+import Head from "next/head";
 
 export default function index() {
   return (
     <div className="index">
+      <Head>
+        <title>Kanaiyali</title>
+        <meta name="description" content="By Arignar Anna Thamizh Mandram" />
+        <link rel="icon" href="/assets/favicon.ico" />
+      </Head>
       <Navbar />
       <HomeScreen />
       <HomeContent />
